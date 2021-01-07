2bce9dbe2432762ab20ee803a16ca77ebb2b09a5
Vijayabalaji B <jayb2495@gmail.com>
2021-01-07 22:13:05 +0530

    EOD commit

---
+++ 09-Data-Structures-Operators/challenge 2/script.js
@@ -0,0 +1,96 @@
+'use strict';
+
+// tasks:
+// 1. Loop over the game.scored array and print each player name to the console,
+// along with the goal number (Example: "Goal 1: Lewandowski")
+// 2. Use a loop to calculate the average odd and log it to the console (We already
+// studied how to calculate averages, you can go check if you don't remember)
+// 3. Print the 3 odds to the console, but in a nice formatted way, exactly like this:
+// Odd of victory Bayern Munich: 1.33
+// Odd of draw: 3.25
+// Odd of victory Borrussia Dortmund: 6.5
+// Get the team names directly from the game object, don't hardcode them
+// (except for "draw"). Hint: Note how the odds and the game objects have the
+// same property names �
+// 4. Bonus: Create an object called 'scorers' which contains the names of the
+// players who scored as properties, and the number of goals as the value. In this
+// game, it will look like this:
+// {
+//  Gnarby: 1,
+//  Hummels: 1,
+//  Lewandowski: 2
+// }
+
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
+
+//1
+const scored = [...game.scored];
+for (const [c, p] of scored.entries()) {
+  console.log(`Goal ${c + 1}: ${p}`);
+}
+//2
+const avg = Object.values(game.odds);
+let average = 0;
+for (const a of avg) average += a;
+
+average = average / avg.length;
+
+console.log(average);
+//3
+const odd = Object.entries(game.odds);
+console.log(odd);
+for (const [team, o] of odd) {
+  let out = game[team]
+    ? `Odd of victory ${game[team]}: ${o}`
+    : `Odd of draw: ${o}`;
+  console.log(`${out}`);
+}
+
+//bonus
+
+const scoredPlayers = {};
+
+for (const player of game.scored) {
+  scoredPlayers[player] ? scoredPlayers[player]++ : (scoredPlayers[player] = 1);
+}
+
+console.log(scoredPlayers);
--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -1,5 +1,20 @@
 'use strict';
 
+const openingHours = {
+  thu: {
+    open: 12,
+    close: 22,
+  },
+  fri: {
+    open: 11,
+    close: 23,
+  },
+  sat: {
+    open: 0, // Open 24 hours
+    close: 24,
+  },
+};
+
 const restaurant = {
   name: 'Classico Italiano',
   location: 'Via Angelo Tavanti 23, Firenze, Italy',
@@ -7,18 +22,30 @@ const restaurant = {
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
-  openingHours: {
-    thu: {
-      open: 12,
-      close: 22,
-    },
-    fri: {
-      open: 11,
-      close: 23,
-    },
-    sat: {
-      open: 0, // Open 24 hours
-      close: 24,
-    },
+  order(start, main) {
+    return [this.starterMenu[start], this.mainMenu[main]];
   },
+
+  openingHours,
 };
+const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+
+for (const item of menu) console.log(item);
+
+for (const [i, el] of menu.entries()) {
+  console.log(`${i}: ${el}`);
+}
+
+console.log(restaurant.openingHours?.fri?.open); //optional chaining
+
+//LOOPING OBJECTS< KEYS AND ENTRIES
+console.log(Object.values(openingHours));
+for (const day of Object.keys(openingHours)) console.log(day);
+
+const entries = Object.entries(openingHours);
+console.log(...entries[0]);
+// [key, { open, close }]
+for (const [key, { open, close }] of entries) {
+  // console.log(x);
+  console.log(`on ${key} open at ${open} and close at ${close}`);
+}
